584a1b568c9e453cd715261561b19d9c8b64a49c
mateus <mateusvanhalen@gmail.com>
2019-03-28 14:23:28 -0500

    locale

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,5 +1,5 @@
 eventsApp.factory('eventData', function($resource) {
-    var resource = $resource('/data/event/:id', {id:'@id'});
+    var resource = $resource('/data/event/:id', {id:'@id'}, {"getAll": {method: "GET", isArray:true, params: {something: "foo"}}});
     return {
         getEvent: function() {
             return resource.get({id:1});
