fac56199ff54ea41ca4736f0f75576daeb2fb0ed
mateus <mateusvanhalen@gmail.com>
2019-03-28 13:42:05 -0500

    write save

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,10 +3,17 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
 
-           eventData.getEvent(function(event) {
-                $scope.event = event;
-            });
+        eventData.getEvent()
+            .$promise
+            .then(function(event) {$scope.event = event;  })
+                .catch(function(response) {console.log(response); }
+            );
 
+        // HTTP resource to be replaced with $Resource service
+        //    .success(function(event) { $scope.event = event; })
+        //    .error(function(data, status, headers, config) {
+        //     $log.warn(data, status, headers(), config);
+        // });
 
             $scope.upVoteSession = function(session) {
                 session.upVoteCount++;
